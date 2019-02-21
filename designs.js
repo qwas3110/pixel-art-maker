4fce4cd3ed429539787a1a11d5ab4f939c8d02ce
qwas3110 <34387621+qwas3110@users.noreply.github.com>
2019-02-21 19:50:47 +0800

    代码优化

--- designs.js
+++ designs.js
@@ -1,73 +1,33 @@
-// Select color input
-const colorPicker = document.querySelector("#colorPicker");
 
-// Select size input
-const inputHeight =  document.querySelector("#inputHeight");
-const inputWidth = document.querySelector("#inputWidth");
+(function makeGrid() {
 
-// Select from submit and pixelCanvas Table
-
-const sizePicker = document.querySelector("#sizePicker");
-const pixelCanvas = document.querySelector("#pixelCanvas");
-
-// When size is submitted by the user, call makeGrid()
-
-function makeGrid(height,width,colorSelect) {
-
-    sizePicker.addEventListener("submit", function (e) {
+  document
+      .querySelector("#sizePicker")
+      .addEventListener("submit", function (e) {
         e.preventDefault();
-        pixelCanvas.innerHTML = "";
-        let h,w;
-        h = height.value;
-        w = width.value;
-        console.log(h,w);
-        for (let x = 0;  x < h; x++) {
-            let tr = document.createElement('tr');
-            for (let i = 0; i < w; i++) {
-                let td = document.createElement("td");
-                tr.appendChild(td);
-                pixelCanvas.appendChild(tr);
-            }
+        document.querySelector("#pixelCanvas").innerHTML = '';
+        const inputHeight = document.querySelector("#inputHeight");
+        const inputWidth =  document.querySelector("#inputWidth");
+        const color = document.querySelector("#colorPicker");
+        const pixelCanvas =  document.querySelector("#pixelCanvas");
+        const result = document.createDocumentFragment();
+        const startingTime = performance.now();
+        for (let row = 0; row < inputHeight.value; row++) {
+          let tr = document.createElement('tr');
+          console.log(`${row + 1}`);
+          for (let col = 0; col < inputWidth.value; col++) {
+            let td = document.createElement('td');
+            td.innerText = col + 1;
+            td.onclick = (e) => e.target.style.backgroundColor = color.value;
+            tr.appendChild(td);
+            result.appendChild(tr);
+          }
         }
-
-        let td = document.querySelectorAll("td");
-        for (let x = 0;  x < td.length; x++) {
-            td[x].addEventListener('click', function (e) {
-                let color = colorSelect.value;
-                td[x].style.backgroundColor = color;
-            })
-        }
-    })
-}
+        pixelCanvas.appendChild(result);
+        const endingTime = performance.now();
+        console.log('This code took ' + (endingTime - startingTime) + ' milliseconds.');
+      })
+}());
 
 
 
-makeGrid(inputHeight,inputWidth,colorPicker);
-
-
-// (function makeEvent() {
-//     const SIZE_PICKER = document.getElementById("sizePicker");
-//     SIZE_PICKER.addEventListener("submit", function() {
-//         event.preventDefault();
-//         var height = document.getElementById("inputHeight").value;
-//         var width = document.getElementById("inputWidth").value;
-//         makeGrid(height, width);
-//     }, false);
-// })();
-//
-// function paintBox() {
-//     const COLOR_PICKER = document.getElementById("colorPicker").value;
-//     this.style.background = COLOR_PICKER
-// }
-//
-// function makeGrid(height, width) {
-//     const TABLE = document.getElementById("pixelCanvas");
-//     TABLE.innerHTML = "";
-//     for (var i = 0; i < height; i++) {
-//         var row = TABLE.insertRow(i);
-//         for (var j= 0; j < width; j++ ) {
-//             var cell = row.insertCell(j);
-//             cell.onclick = paintBox;
-//         }
-//     }
-// }
