68eda1f0b269a80bebe941350bb842902ce5b017
qwas3110 <283994917@qq.com>
2019-03-16 14:49:16 +0800

    optimization

--- designs.js
+++ designs.js
@@ -1,33 +1,30 @@
 
+
 (function makeGrid() {
+    const color = document.getElementById('colorPicker');
+    const inputHeight = document.getElementById('inputHeight');
+    const inputWidth = document.getElementById('inputWidth');
+    const pixelCanvas = document.getElementById('pixelCanvas');
 
-  document
-      .querySelector("#sizePicker")
-      .addEventListener("submit", function (e) {
-        e.preventDefault();
-        document.querySelector("#pixelCanvas").innerHTML = '';
-        const inputHeight = document.querySelector("#inputHeight");
-        const inputWidth =  document.querySelector("#inputWidth");
-        const color = document.querySelector("#colorPicker");
-        const pixelCanvas =  document.querySelector("#pixelCanvas");
-        const result = document.createDocumentFragment();
-        const startingTime = performance.now();
-        for (let row = 0; row < inputHeight.value; row++) {
-          let tr = document.createElement('tr');
-          console.log(`${row + 1}`);
-          for (let col = 0; col < inputWidth.value; col++) {
-            let td = document.createElement('td');
-            td.innerText = col + 1;
-            td.onclick = (e) => e.target.style.backgroundColor = color.value;
-            tr.appendChild(td);
-            result.appendChild(tr);
-          }
-        }
-        pixelCanvas.appendChild(result);
-        const endingTime = performance.now();
-        console.log('This code took ' + (endingTime - startingTime) + ' milliseconds.');
-      })
+    document
+        .getElementById('sizePicker')
+        .addEventListener('submit' , function (e) {
+            e.preventDefault();
+            pixelCanvas.innerHTML = '';
+            const result = document.createDocumentFragment();
+
+            for (let row = 0; row < inputHeight.value; row++) {
+                let tr = document.createElement('tr');
+                for (let col = 0; col < inputWidth.value; col++) {
+                    let td = document.createElement('td');
+                    td.textContent = col + 1;
+                    td.onclick = (e) => {
+                        e.target.style.backgroundColor = color.value;
+                    };
+                    tr.appendChild(td);
+                    result.appendChild(tr);
+                }
+            }
+            pixelCanvas.appendChild(result);
+        })
 }());
-
-
-
